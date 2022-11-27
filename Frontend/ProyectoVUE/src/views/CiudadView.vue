7e31cab688d6a4eabd9715c1643c57897098bed5
Karolhzz <carolina.hernandez@uptc.edu.co>
2022-11-27 18:56:03 -0500

    Frontend

---
+++ Frontend/ProyectoVUE/src/views/CiudadView.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>{{texto}}</h1>
+    <h3>CIUDAD</h3>
+</template>
+
+
+<script>
+
+
+export default{
+    data() {
+        return {
+            texto: "ciudad"
+   
+        }
+    }
+    
+}
+
+</script>
+
+
+<style scoped>
+</style>
