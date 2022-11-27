1d83037ffc27c04966f600c2ebda47351e1f7bb3
jasfas <javier210186@hotmail.com>
2022-11-27 18:07:11 -0500

    fronted

---
+++ ProyectoVUE/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container">
+        <div class="row">
+        <div class="col-md-12">
+            
+        <Encabezado /> 
+        </div>
+        </div>
+
+        <div class="row">
+            <ul class="nav">
+            <li class="nav-item">
+
+            <RouterLink class="nav-link active" to="/">Home</RouterLink>
+            </li>
+            <li class="nav-item">
+            <RouterLink class="nav-link" to="/persona"> Persona</RouterLink></li>
+            <li class="nav-item">
+            <RouterLink class="nav-link " to="/ciudad">Ciudad</RouterLink>
+            </li>
+            </ul>
+        </div>
+
+        <div class="row contenido">
+            <RouterView />
+         </div>
+
+        <div class="row">
+            
+            <Pie/>
+        </div>
+    </div>
+
+    </template>
+
+
+
+    <script>
+    import { RouterLink, RouterView } from 'vue-router';
+
+    import Encabezado from './components/Encabezado.vue';
+    import Pie from './components/Pie.vue';
+  
+
+    export default{
+    components: { Encabezado, Pie },
+    data() {
+        return{
+            mensaje:"hola mundo",
+        }
+    }
+    }
+
+
+    
+    </script>
+
+
+
+    <style scoped>
+    .contenido {
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    min-height: 550px;
+    }
+    </style>
---
+++ ProyectoVUE/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row" >
+        <div class="col-md8">
+            <Home/>
+        </div>
+        <div col-md4>
+            <Lateral/>
+        </div>
+<h3>HOMEVIEW</h3>
+    </div>
+</template>
+
+
+<script>
+
+import Home from '../components/Home.vue';
+import Lateral from '../components/Lateral.vue';
+
+export default{
+    
+    components: { Home, Lateral },
+}
+
+</script>
+
+
+<style scoped>
+</style>
