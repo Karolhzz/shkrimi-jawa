6bf71d1445d8f0e94dbf64244a5e4a3a84b3ab6e
Karolhzz <carolina.hernandez@uptc.edu.co>
2022-12-09 12:50:33 -0500

    Frontend

---
+++ ProyectoVUE/src/components/Encabezado.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row" >
+        <img :src="logo" alt="Logotipo del ejemplo">
+        <h1>{{titulo}}</h1>
+
+    </div>
+</template>
+
+
+<script>
+
+export default{
+    data() {
+        return{
+            titulo:"Prueba de encabezado",
+            logo:"https://www.ejemplos.co/wp-content/uploads/2015/11/Logo-Adidas.jpg"
+
+        }
+    }
+    }
+
+
+
+</script>
+
+
+<style scoped>
+</style>
---
+++ ProyectoVUE/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="col-6" >
+        <h2> {{texto}}</h2>
+        <h3>HOME.VUE</h3>
+    </div>
+</template>
+
+
+<script>
+    export default{
+        data(){
+         return{
+            texto: "Este es el contenido del home"
+        }
+    }
+}
+
+</script>
+
+
+<style scoped>
+</style>
